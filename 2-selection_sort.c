71c0455f46b7dc0205056e9b14288d0e838cfdcd
Omo <omoikheloa56@gmail.com>
2024-02-20 07:06:17 -0800

    code update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,21 +12,19 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int temp;
 
-	if (!array || size < 2)
+	if (!array || !size)
 		return;
-
-
+	
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
-
-		for (j = i + 1; j < size; j++)
+		for (j = size - 1, min_index = i + 1; j > i; j--)
 		{
 			if (array[j] < array[min_index])
+			{
 				min_index = j;
+			}
 		}
-
-		if (min_index != i)
+		if (array[i] > array[min_index])
 		{
 			temp = array[i];
 			array[i] = array[min_index];

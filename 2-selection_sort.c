572fd51eca5c5caaf03ab4fc5b0e845d2b1af83c
Omo <omoikheloa56@gmail.com>
2024-02-20 05:50:46 -0800

    betty check

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,7 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
 	int temp;
-	
+
 	if (!array || size < 2)
 		return;
 
@@ -19,13 +19,13 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
-		
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
 		}
-		
+
 		if (min_index != i)
 		{
 			temp = array[i];
